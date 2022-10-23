a2a46d9cb0a0cd4d5ce3feea90955be7c3119315
t_ishikawa <tissi0708@gmail.com>
2022-10-23 23:54:56 +0900

    add main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/simultechnology/my_go_todo_app
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	fmt.Println("start web server")
+
+	err := http.ListenAndServe(
+		":18080",
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	)
+	if err != nil {
+		fmt.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
+	}
+
+}
